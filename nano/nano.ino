58d5ab9d00a8ae0248ceb329cd5a217d560bb8e9
LennartKn <40372565+LennartKn@users.noreply.github.com>
2018-06-18 22:20:50 +0000

    First Code for the Arduino Nano

---
+++ nano/nano.ino
@@ -0,0 +1,27 @@
+const int xpin = 0;
+const int ypin = 1;
+const int SW_pin = 9;
+
+void setup() {
+ 
+  Serial.begin(9600);
+
+  pinMode(SW_pin, INPUT);
+  //digitalWrite(SW_pin, HIGH);
+}
+
+void loop() {
+  if(analogRead(xpin) > 700)
+    Serial.write('1');
+  else if(analogRead(xpin) < 400)
+    Serial.write('2');
+  else if(analogRead(ypin) > 700)
+    Serial.write('3');
+  else if(analogRead(ypin) < 400)
+    Serial.write('4');
+  else if(digitalRead(SW_pin))
+    Serial.write('5');
+   
+  delay(50);
+
+}
